5029ab712018e39be0fa8023e170e2a5176f37fb
Alexey <61322192+alexey-cspr@users.noreply.github.com>
2020-03-24 19:35:27 +0300

    Update tasks.py

--- Solutions/Task1/853502_alexey_ovchinnikov/tasks.py
+++ Solutions/Task1/853502_alexey_ovchinnikov/tasks.py
@@ -3,30 +3,22 @@ import argparse
 import re
 
 
-def word_count(task, string):
-    dict_ = {}
-    lst = string.replace('.', ' ').split()
-    # lst = re.split('[^a-zA-Zа-яА-Я0-9]', string)
-    for i in lst:
-        dict_[i] = 0
-        for j in lst:
-            if i == j:
-                dict_[i] += 1
-    if task == 'count':
-        print(dict_.items())
-    elif task == 'repeat':
-        for j in range(len(dict_)):
-            if j == 10:
-                break
-            max = lst[0]
-            for i in dict_:
-                if dict_[max] <= dict_[i]:
-                    max = i
-                else:
-                    while lst.count(max):
-                        lst.remove(max)
-            print(str(max) + '->' + str(dict_[max]))
-            dict_.pop(max)
+def word_count(text: str) -> dict:
+    words = re.split('[^a-zA-Zа-яА-Я0-9]', text)
+    stats = dict()
+    for word in words:
+        if word.lower() in stats.keys():
+            stats[word.lower()] += 1
+        else:
+            stats[word.lower()] = 1
+    stats.pop('')
+    return stats
+
+def repeat(words: dict) -> str:
+    sorted_values = [value[0] for value in sorted(words.items(), key=lambda x: x[1], reverse=True)]
+    sorted_values[0] = sorted_values[0].capitalize()
+    sorted_values[len(sorted_values)-1] += '.'
+    return ' '.join(sorted_values)
 
 def quick_sort(arr, fst, lst):
     f = fst
@@ -80,6 +72,7 @@ def fibonacci(max):
         yield a
         a, b = b, a + b
 
+
 def open_text(file_name):
     with open(file_name) as file:
         string = file.read()
@@ -93,29 +86,37 @@ def open_data(file_name):
     return arr
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p','--problem', choices=['count', 'repeat', 'quick_sort', 'merge_sort', 'fibonacci'])
+parser.add_argument('-p','--problem', choices=['count', 'sort', 'fibonacci'], default='count')
+parser.add_argument('-s','--sort', choices=['quick', 'merge'])
 parser.add_argument('-fn', '--filename', choices=['text_data.txt', 'data.txt'], default='data.text')
+parser.add_argument('-fib', '--fibonacci', choices=['20', '50', '100', '150'], default='8')
 args = parser.parse_args()
 problem = args.problem
+fib_num = args.fibonacci
+sort = args.sort
 file_name = args.filename
 
 if problem == 'count' or problem == 'repeat':
     string = open_text(file_name)
     if problem == 'count':
-        word_count(problem, string)
-    else:
-        word_count(problem, string)
-elif problem == 'quick_sort' or problem == 'merge_sort':
+        stats = word_count(string)
+    for (key, value) in stats.items():
+            print(key, ':', value)
+    print('Sentence made of the most common words in sequence:', repeat(stats))
+    # else:
+    #     word_count(problem, string)
+elif problem == 'sort':
     arr = open_data(file_name)
-    if problem == 'quick_sort':
+    if sort == 'quick':
         if arr[0] > arr[len(arr)-1]:
             arr[0], arr[len(arr)-1] = arr[len(arr)-1], arr[0]
         quick_sort(arr, arr[0], len(arr)-1)
         print(arr)
     else:
         arr = merge_sort(arr)
-        print(arr)
+        print(arr)  
 elif problem == 'fibonacci':
-    num = random.randint(5,15)
-    for n in fibonacci(num):
+    # num = random.randint(20,25)
+    for n in fibonacci(int(fib_num)):
         print(n)
+
