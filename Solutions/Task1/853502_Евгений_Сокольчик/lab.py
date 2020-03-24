663d4dc4c2b6d3d61f5daac3419bbf85ab506dd9
ChoCoBo1712 <61051642+ChoCoBo1712@users.noreply.github.com>
2020-03-24 18:21:18 +0300

    Update lab.py

--- Solutions/Task1/853502_Евгений_Сокольчик/lab.py
+++ Solutions/Task1/853502_Евгений_Сокольчик/lab.py
@@ -1,15 +1,15 @@
 import re
 import argparse
 
-def generator(first = 0, second = 1):
-    while second < 100:
+def generator(range, first = 0, second = 1):
+    while second < range:
         temp = second
         second += first
         first = temp
         yield first
 
-def fibonacci():
-    f = generator()
+def fibonacci(range):
+    f = generator(range)
     for num in f:
         print(num)
 
@@ -44,9 +44,9 @@ def quick_sort(list, first, last):
     i,j = first, last
     pivot = list[(first + last) // 2]
     while i <= j:
-        while list[i] < pivot and i < last:
+        while list[i] < pivot:
             i += 1
-        while list[j] > pivot and j > first:
+        while list[j] > pivot:
             j -= 1
         if i <= j:
             list[i], list[j] = list[j], list[i]
@@ -57,7 +57,7 @@ def quick_sort(list, first, last):
     if i < last:
         quick_sort(list, i, last)
 
-def text(task, file):
+def text(task, file, length = 0):
     with open(file, "r") as text:
         regexp = re.findall(r"\w+", text.read())
     words = dict()
@@ -68,16 +68,20 @@ def text(task, file):
         else:
             words[word] = 1
     if task == "list":
-        for item in words.items():
-            print(str(item[1]) + " " + item[0])
+        return words
     else:
         words = sorted(words.items(), key=lambda pair: pair[1], reverse = True)
         sentence = []
-        for word in list(words) [:10]:
+        if length > len(words):
+            print("Max length is " + str(len(words)) + ".")
+            length = len(words)
+        elif length <= 0:
+            return "Invalid length."
+        for word in list(words) [:length]:
             sentence.append(word[0])
         sentence[0] = sentence[0].capitalize()
-        sentence[9] += "."
-        print(" ".join(sentence))
+        sentence[length - 1] += "."
+        return " ".join(sentence)
 
 def sort(task, file):
     with open(file, "r") as nums:
@@ -93,16 +97,23 @@ def sort(task, file):
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--task", choices=["list", "sentence", "quick_sort", "merge_sort", "fibonacci"], default="fibonacci")
-    parser.add_argument("-f", "--file", default="text.txt")
+    parser.add_argument("-f", "--file", choices=["text.txt", "numbers.txt"], default="text.txt")
+    parser.add_argument("-sl", "--sentence_length", default=10, type=int)
+    parser.add_argument("-fr", "--fib_range", default=100, type=int)
     return parser
 
 parser = create_parser()
 params = parser.parse_args()
 task = params.task
 file = params.file
-if task == "list" or task == "sentence":
-    text(task, file)
+sentence_length = params.sentence_length
+fib_range = params.fib_range
+if task == "list":
+    for item in text(task, file).items():
+        print(str(item[1]) + " " + item[0])
+elif task == "sentence":
+    print(text(task, file, sentence_length))
 elif task == "quick_sort" or task == "merge_sort":
     sort(task, file)
 else:
-    fibonacci()
+    fibonacci(fib_range)
